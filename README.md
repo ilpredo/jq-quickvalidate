b14352f53e8ab7fa38a2d3a9648c835dfa1e3bf7
elclanrs <elclanrs@gmail.com>
2012-04-20 05:44:32 -0400

    cleanup added README
    cleanup
    
    cleanup

---
+++ README.md
@@ -0,0 +1,66 @@
+# jq-quickvalidate
+
+A small & extensible jQuery validation plugin.
+
+**Demo:** http://jsfiddle.net/elclanrs/ZsS2D/
+
+* * *
+
+
+# How to use quickValidate:
+
+Load the latest [jQuery library](http://jquery.com), the `jq-quickvalidate.js` plugin and the `jq-quickvalidate.css` stylesheet into your project as well as the [`normalize.css`](http://necolas.github.com/normalize.css/) reset. Feel free to customize the CSS to your needs if you need to adjust the layout.
+
+## HTML:
+
+    <form id="my-form" action="action.php" method="post">
+
+        <p><label>Name <span>*</span>:</label><input type="text" class="required name"/></p>
+        
+        ...
+
+    </form>
+      
+**Default filters:**
+
+You may use the `required` filter plus ONE other filter from the list. 
+
++   required
++   number
++   name
++   username
++   pass
++   strongpass
++   email
++   phone
++   zip
++   date
+
+## JavaScript:
+
+    $('#my-form').quickValidate({
+        speed: 'fast' // Popup fade speed
+        easing: 'linear' // Popup easing (requires easing plugin)
+        onSubmit: function(){
+            // What happens when the user submits
+            // the form and does NOT validate
+        },
+        filters: {
+            // Add new filters or overwrite the default 
+            // ones following this pattern
+            custom: {
+                regex: /something/,
+                error: 'Custom error message.'
+            }
+            another: {
+                // `regex` also takes a function
+                // with the input's `value` as only
+                // parameter. The function must
+                // return a boolean or falsy value
+                regex: function(value){
+                    var re = /hello world/i;
+                    return re.test(value); // returns boolean
+                }
+            }
+        }
+    });
--- js/jq-quickvalidate.js
+++ js/jq-quickvalidate.js
@@ -4,7 +4,7 @@
 
     * Author: Cedric Ruiz
     * License: GPLv2
-    * Website: http://code.google.com/p/jq-quickvalidate/
+    * Website: https://github.com/elclanrs/jq-quickvalidate
 
 -------------------------------------------------------- */
 
@@ -12,27 +12,27 @@
     
     'use strict';
     
-	$.fn.quickValidate = function (ops) {
+    $.fn.quickValidate = function (ops) {
     
         // Default options
-		var o = $.extend({
-			onSubmit: function () {
+        var o = $.extend({
+            onSubmit: function () {
                 // What happens on submit if the form
                 // does NOT validate.
-				alert('The form does not validate! Check again...');
-			},
-			filters: {
+                alert('The form does not validate! Check again...');
+            },
+            filters: {
                 // Add your own filters
                 // ie. myfilter: { regex: /something/, error: 'My error' }
             },
-			speed: 'fast', // Popup fadeIn speed
-			easing: 'swing' // Popup fadeIn easing
-		}, ops);
+            speed: 'fast', // Popup fadeIn speed
+            easing: 'swing' // Popup fadeIn easing
+        }, ops);
         
         // Cache variables
-		var $form = this,
+        var $form = this,
             // Only process these elements
-			$inputs = $form.find('input:text, input:password');          
+            $inputs = $form.find('input:text, input:password');          
             
 /* --------------------------------------------------------
 
@@ -43,53 +43,53 @@
  
 -------------------------------------------------------- */    
         
-		var filters = {
-			number: {
-				regex: /\d+/,
-				error: 'Must be a number.'
-			},
-			name: {
-				regex: /^[A-Za-z]{3,}$/,
-				error: 'Must be at least 3 characters long, and must only contain letters.'
-			},
-			username: {
-				regex: /^[a-z](?=[\w.]{3,31}$)\w*\.?\w*$/i,
-				error: 'Must be at between 4 and 32 characters long and start with a letter. You may use letters, numbers, underscores, and one dot (.)'
-			},
-			pass: {
-				regex: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/,
-				error: 'Must be at least 6 characters long, and contain at least one number, one uppercase and one lowercase letter.'
-			},
-			strongpass: {
-				regex: /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
-				error: 'Must be at least 8 characters long and contain at least one uppercase and one lowercase letter and one number or special character.'
-			},
-			email: {
-				regex: /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/,
-				error: 'Must be a valid e-mail address. (e.g. user@gmail.com)'
-			},
-			phone: {
-				regex: /^[2-9]\d{2}-\d{3}-\d{4}$/,
-				error: 'Must be a valid US phone number. (e.g. 555-123-4567)'
-			},
-			zip: {
-				regex: /^\d{5}$|^\d{5}-\d{4}$/,
-				error: 'Must be a valid US zip code. (e.g. 33245 or 33245-0003)'
-			},
-			date: {
-				regex: function (value) {
-					var match = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/.exec(value),
+        var filters = {
+            number: {
+                regex: /\d+/,
+                error: 'Must be a number.'
+            },
+            name: {
+                regex: /^[A-Za-z]{3,}$/,
+                error: 'Must be at least 3 characters long, and must only contain letters.'
+            },
+            username: {
+                regex: /^[a-z](?=[\w.]{3,31}$)\w*\.?\w*$/i,
+                error: 'Must be at between 4 and 32 characters long and start with a letter. You may use letters, numbers, underscores, and one dot (.)'
+            },
+            pass: {
+                regex: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/,
+                error: 'Must be at least 6 characters long, and contain at least one number, one uppercase and one lowercase letter.'
+            },
+            strongpass: {
+                regex: /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+                error: 'Must be at least 8 characters long and contain at least one uppercase and one lowercase letter and one number or special character.'
+            },
+            email: {
+                regex: /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/,
+                error: 'Must be a valid e-mail address. (e.g. user@gmail.com)'
+            },
+            phone: {
+                regex: /^[2-9]\d{2}-\d{3}-\d{4}$/,
+                error: 'Must be a valid US phone number. (e.g. 555-123-4567)'
+            },
+            zip: {
+                regex: /^\d{5}$|^\d{5}-\d{4}$/,
+                error: 'Must be a valid US zip code. (e.g. 33245 or 33245-0003)'
+            },
+            date: {
+                regex: function (value) {
+                    var match = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/.exec(value),
                         isDate = function (m, d, y) {
                             return m > 0 && m < 13 && y > 0 && y < 32768 && d > 0 && d <= (new Date(y, m, 0)).getDate();
                         };
-					return (match) ? isDate(match[1], match[2], match[3]) : null;
-				},
-				error: 'Must be a valid date. (e.g. mm/dd/yyyy)'
-			}
-		};
+                    return (match) ? isDate(match[1], match[2], match[3]) : null;
+                },
+                error: 'Must be a valid date. (e.g. mm/dd/yyyy)'
+            }
+        };
         
         // Merge custom and default filters
-		$.extend(true, filters, o.filters);
+        $.extend(true, filters, o.filters);
         
 /* --------------------------------------------------------
 
@@ -100,13 +100,13 @@
  
 -------------------------------------------------------- */
         
-		var validate = function (klass, value) {
-			var isValid = true,
-				error = '';
-			if (!value && /required/.test(klass)) {
-				error = 'This field is required.';
-				isValid = false;
-			}
+        var validate = function (klass, value) {
+            var isValid = true,
+                error = '';
+            if (!value && /required/.test(klass)) {
+                error = 'This field is required.';
+                isValid = false;
+            }
             if (value) {
                 klass = klass.split(/\s/);
                 $.each(klass, function(i, k){
@@ -120,12 +120,12 @@
                         }
                     }
                 });
-			}
-			return {
-				isValid: isValid,
-				error: error
-			};
-		};
+            }
+            return {
+                isValid: isValid,
+                error: error
+            };
+        };
         
 /* --------------------------------------------------------
 
@@ -135,25 +135,25 @@
  
 -------------------------------------------------------- */
         
-		var analyze = function ($input) {
-			var klass = $input.attr('class'),
-				value = $input.val(),
-				test = validate(klass, value),
-				$error = $('<span class="error">' + test.error + '</span>'),
-				$info = $('<i class="error-icon"></i>'),
-				$ok = $('<i class="valid-icon"></i>');
+        var analyze = function ($input) {
+            var klass = $input.attr('class'),
+                value = $input.val(),
+                test = validate(klass, value),
+                $error = $('<span class="error">' + test.error + '</span>'),
+                $info = $('<i class="error-icon"></i>'),
+                $ok = $('<i class="valid-icon"></i>');
             $info.hover(function () {
                 $(this).siblings('.error').stop(1, 1).fadeToggle(o.speed, o.easing);
             });    
-			$input.removeClass('invalid').siblings('.error, .error-icon, .valid-icon').remove();
-			if (!test.isValid) {
-				$input.addClass('invalid');
-				$error.add($info).insertAfter($input);
-			}
+            $input.removeClass('invalid').siblings('.error, .error-icon, .valid-icon').remove();
+            if (!test.isValid) {
+                $input.addClass('invalid');
+                $error.add($info).insertAfter($input);
+            }
             if (value && test.isValid) {
-				$ok.insertAfter($input);
-			}
-		};
+                $ok.insertAfter($input);
+            }
+        };
         
 /* --------------------------------------------------------
 
@@ -161,7 +161,7 @@
  
 -------------------------------------------------------- */   
             
-		$inputs
+        $inputs
             .each(function () {
                 if ($(this).is('.required')) {
                     analyze($(this));
@@ -171,12 +171,12 @@
                 analyze($(this));
             });
 
-		$form.submit(function (e) {
-			if ($form.find('input.invalid').length) {
-				e.preventDefault();
-				o.onSubmit();
-			}
-		});
-		return this;
-	};
+        $form.submit(function (e) {
+            if ($form.find('input.invalid').length) {
+                e.preventDefault();
+                o.onSubmit();
+            }
+        });
+        return this;
+    };
 }(jQuery));
